89f0d7576839b8c644a73a3cf4c53657c0fcfac6
Jeff Everhart <jeffeverhart383@gmail.com>
2016-09-25 19:31:49 -0400

    update files

--- writefile.py
+++ writefile.py
@@ -1,4 +1,14 @@
 import json
+import csv
+
+csvfile = open("text.csv", "a")
+csvfieldnames = ['first', 'second']
+writer = csv.DictWriter(csvfile, csvfieldnames)
+writer.writeheader()
+writer.writerow({'first':'this thing', 'second':'this other thing'})
+csvfile.close()
+
+
 
 textfile = open("test.txt", "a")
 
